f584fc8431b5837a61b41d506e13873ad3d7c0e0
thoparamvishwateja <thoparamvishwateja0000@gmail.com>
2024-04-03 20:23:28 +0530

    second updated

--- src/assert/api.js
+++ src/assert/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 //   baseURL: 'http://localhost:8080/api', // Example base URL
-    baseURL: 'authentication-backend-production-ddd4.up.railway.app',
+    baseURL: 'http://authentication-backend-production-985f.up.railway.app',
 //   timeout: 5000, // Example timeout configuration
 });
 
